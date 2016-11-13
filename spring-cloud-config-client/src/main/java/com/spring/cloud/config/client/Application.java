1cca262b2461375ad05db49716f467e8efa1d005
repoofshichao <shichaobox@163.com>
2016-11-13 19:07:32 +0800

    init

---
+++ spring-cloud-config-client/src/main/java/com/spring/cloud/config/client/Application.java
@@ -0,0 +1,14 @@
+package com.spring.cloud.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created on 2016/11/13 013.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
